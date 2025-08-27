8fb5199bdb2866906623f69748638e0b9b8468e9
yaffy aiger <yafa6105@gmail.com>
2025-08-27 13:17:16 +0300

    ארגון מחדש: העברת כל קבצי הפרויקט לתיקיית backend, מחיקת node_modules מהשורש, עדכון .gitignore

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/YaffyAiger/accountancy';
+  const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/accountancy';
     await mongoose.connect(mongoURI);
     console.log('MongoDB connected');
   } catch (error) {
